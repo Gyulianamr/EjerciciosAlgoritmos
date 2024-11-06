c2759aed2db03ee0e7fff3a329c7ee1c5c7985fe
Gyulianamr <genesismedina0529@gmail.com>
2024-11-06 14:25:55 -0600

    Add files via upload

--- Examen/ColaEspecial.py
+++ Examen/ColaEspecial.py
@@ -21,10 +21,21 @@ class ColaEspecial(object):
         print(self.cola) 
             
     def __str__(self):
-        return f"Arreglo({self.cola})"
+        ans = "["
+        for i in range(self.nItems):
+            if len(ans) > 1:
+                ans += ", "
+            j = i + self.cola
+            if j >= self.nItems:
+                j -= self.nItems
+            ans += str(self.cola[j])
+        ans += "]"
+        return ans
+    
     
             
     def eliminar(self, dato):
         self.cola.pop(dato)
         
+
 
